80dc23a4034edc664a80183d304d2c1141eb673d
Fer Sánchez <sangarfer@gmail.com>
2022-03-03 12:26:29 +0100

    morning class pause

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
-import Counter from "./components/01-use-state/Counter";
-// import Timer from "./components/02-use-state-loop/Timer";
+import Counter from "./components/Counter";
+import Timer from "./components/Timer";
 // import TimerTwo from "./components/03-use-effect-mounting/TimerTwo";
 // import TimerThree from "./components/04-use-effect-unmounting/TimerThree";
 // import TimerFour from "./components/05-use-effect-conditional-updates/TimerFour";
@@ -16,7 +16,7 @@ function App() {
       <button onClick={() => setShow(!show)}>
         { show ? "Hide" : "Show"}
       </button>
-      {show && <Counter />}
+      {show && <Timer />}
     </div>
   );
 }
